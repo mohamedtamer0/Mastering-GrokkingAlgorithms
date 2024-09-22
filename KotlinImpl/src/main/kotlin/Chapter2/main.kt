2813564dfa4541a11356430be910609dac6e9abd
mohamedtamer0 <mt4452242@gmail.com>
2024-09-22 19:50:12 +0300

    update

---
+++ KotlinImpl/src/main/kotlin/Chapter2/main.kt
@@ -0,0 +1,16 @@
+package Chapter2
+
+fun main() {
+    // Creating an array
+    val numbers = arrayOf(1, 2, 3, 4, 5)
+
+    // Reading elements
+    for (i in numbers.indices) {
+        println("Element at index $i: ${numbers[i]}")
+    }
+
+    // Inserting an element at the end
+    val list = numbers.toMutableList() // Convert array to a mutable list
+    list.add(6) // Adding an element at the end
+    println("Elements after addition: $list")
+}
