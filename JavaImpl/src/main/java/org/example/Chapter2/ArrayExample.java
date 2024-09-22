51f73de24011de3ea9aff8f847931313bed680e1
mohamedtamer0 <mt4452242@gmail.com>
2024-09-22 19:48:55 +0300

    update

---
+++ JavaImpl/src/main/java/org/example/Chapter2/ArrayExample.java
@@ -0,0 +1,23 @@
+package org.example.Chapter2;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Creating an array
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Reading elements
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+
+        // Inserting an element at the end (in case of dynamic array)
+        Integer[] integerArray = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
+        list.add(6); // Adding an element at the end
+        System.out.println("Elements after addition: " + list);
+    }
+}
